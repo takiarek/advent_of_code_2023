19e760f89511268ca3be7e5c99d65796b6f758cb
Arek Kita <arkadiusz.kita@lunarlogic.io>
2023-12-13 11:18:00 +0100

    Inline getting Line value

--- part_1.rb
+++ part_1.rb
@@ -21,7 +21,7 @@ class EngineSchematic
   private
 
   def lines
-    @lines.map { |line| Line.new(line).value }
+    @lines.map { |line| Line.new(line) }
   end
 
   def find_potential_parts
@@ -29,8 +29,8 @@ class EngineSchematic
       hash[line_index] = numbers.map do |number|
         {
           number: number,
-          start_index: lines[line_index].index(number),
-          end_index: lines[line_index].index(number) + number.length - 1
+          start_index: lines[line_index].value.index(number),
+          end_index: lines[line_index].value.index(number) + number.length - 1
         }
       end
     end
@@ -45,7 +45,7 @@ class EngineSchematic
   end
 
   def lines_numbers
-    lines.map { |line| get_numbers(line) }
+    lines.map { |line| get_numbers(line.value) }
   end
 
   def get_numbers(line)
@@ -55,7 +55,7 @@ class EngineSchematic
   def find_real_parts(lines_with_potential_parts)
     lines_with_potential_parts.map do |line_index, potential_parts|
       potential_parts.select do |part|
-        current_line = lines[line_index]
+        current_line = lines[line_index].value
         next true if symbol?(current_line[part[:start_index] - 1]) || symbol?(current_line[part[:end_index] + 1])
 
         other_lines = case line_index
@@ -68,7 +68,7 @@ class EngineSchematic
         end
 
         other_lines.any? do |other_line|
-          line = lines[line_index + other_line]
+          line = lines[line_index + other_line].value
 
           start = [part[:start_index] - 1, 0].max
           finish = [part[:end_index] + 1, line.length - 1].min
