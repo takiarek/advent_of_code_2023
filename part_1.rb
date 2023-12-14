203dda02a9aa275bed77a17ba4ad617a044b2a65
Arek Kita <arkadiusz.kita@lunarlogic.io>
2023-12-14 13:06:13 +0100

    Change Algorythm & Extract Method

--- part_1.rb
+++ part_1.rb
@@ -23,27 +23,21 @@ class EngineSchematic
   def real_parts
     lines.map.with_index do |current_line, line_index|
       current_line.potential_parts.select do |part|
-        next true if symbol?(current_line.value[part.start_index - 1]) || symbol?(current_line.value[part.end_index + 1])
-
-        other_lines = case line_index
-        when 0
-          [1]
-        when lines.length - 1
-          [-1]
-        else
-          [-1, 1]
-        end
-
-        other_lines.any? do |other_line|
-          line = lines[line_index + other_line].value
-
+        neighbourhood_lines(current_line, line_index).any? do |line|
           start = [part.start_index - 1, 0].max
-          finish = [part.end_index + 1, line.length - 1].min
+          finish = [part.end_index + 1, line.value.length - 1].min
 
-          line[start..finish].split.any? { |char| symbol?(char) }
+          line.value[start..finish].split.any? { |char| symbol?(char) }
         end
       end
     end.flatten
+  end
+
+  def neighbourhood_lines(line, line_index)
+    start = [line_index - 1, 0].max
+    finish = [line_index + 1, lines.length - 1].min
+
+    lines[start..finish]
   end
 
   def symbol?(character)
