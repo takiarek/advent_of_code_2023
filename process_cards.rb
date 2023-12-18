0638122c2bbaffcbf3dc21f0a864674cdc0ca3fe
Arek Kita <arkadiusz.kita@lunarlogic.io>
2023-12-18 12:38:59 +0100

    Extract Method: winning_numbers

--- process_cards.rb
+++ process_cards.rb
@@ -5,7 +5,7 @@ class ProcessCards
 
   def calculate_points
     lines.sum do |line|
-      winning_numbers, my_numbers = winnning_and_my_numbers(line)
+      winning_numbers, my_numbers = [winning_numbers(line), winnning_and_my_numbers(line)[1]]
 
       my_numbers.reduce(0) do |points, my_number|
         next points unless winning_numbers.include?(my_number)
@@ -18,6 +18,10 @@ class ProcessCards
   private
 
   attr_reader :lines
+
+  def winning_numbers(line)
+    winnning_and_my_numbers(line)[0]
+  end
 
   def winnning_and_my_numbers(line)
     line.split(": ")[1].split(" | ").map do |numbers_string|
