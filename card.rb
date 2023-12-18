4485253bab2743845b6bf365006a860c99121129
Arek Kita <arkadiusz.kita@lunarlogic.io>
2023-12-18 15:42:35 +0100

    Remove Dead Code: to_s

--- card.rb
+++ card.rb
@@ -3,10 +3,6 @@ class Card
     @value = value
   end
 
-  def to_s
-    @value
-  end
-
   def winnning_and_my_numbers
     @value.split(": ")[1].split(" | ").map do |numbers_string|
       numbers_string.split(" ")
