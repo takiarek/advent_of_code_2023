9ff657257e8f7838c01eaeb11196ee01a9c953d3
Arek Kita <arkadiusz.kita@lunarlogic.io>
2023-12-18 15:48:35 +0100

    Move Method: my_numbers

--- card.rb
+++ card.rb
@@ -3,6 +3,10 @@ class Card
     @value = value
   end
 
+  def my_numbers
+    winnning_and_my_numbers[1]
+  end
+
   def winnning_and_my_numbers
     @value.split(": ")[1].split(" | ").map do |numbers_string|
       numbers_string.split(" ")
--- process_cards.rb
+++ process_cards.rb
@@ -7,7 +7,7 @@ class ProcessCards
 
   def calculate_points
     cards.sum do |card|
-      my_numbers(card).reduce(0) do |points, my_number|
+      card.my_numbers.reduce(0) do |points, my_number|
         next points unless winning_numbers(card).include?(my_number)
 
         (points == 0) ? points + 1 : points * 2
@@ -23,9 +23,5 @@ class ProcessCards
 
   def winning_numbers(card)
     card.winnning_and_my_numbers[0]
-  end
-
-  def my_numbers(card)
-    card.winnning_and_my_numbers[1]
   end
 end
