8517fb7e7964d80cce7fbd8f2f43e9a80d443448
Arek Kita <arkadiusz.kita@lunarlogic.io>
2023-12-21 11:44:26 +0100

    Solve part 2

--- process_cards.rb
+++ process_cards.rb
@@ -11,9 +11,25 @@ class ProcessCards
     end
   end
 
+  def calculate_cards_won
+    cards.sum do |card|
+      cards_count_for(card)
+    end
+  end
+
   private
 
   def cards
     @cards.map { |card| Card.new(card) }
+  end
+
+  def cards_count_for(card)
+    return 1 if card.matching_numbers_count == 0
+
+    won_copies = cards[card.number..card.number + card.matching_numbers_count - 1]
+
+    won_from_copies_count = won_copies.sum { |copy_card| cards_count_for(copy_card) }
+
+    1 + won_from_copies_count
   end
 end
--- process_cards_test.rb
+++ process_cards_test.rb
@@ -6,4 +6,8 @@ class ProcessCardsTest < Minitest::Test
   def test_calculate_points
     assert_equal(13, ProcessCards.new("../inputs/4/example_input.txt").calculate_points)
   end
+
+  def test_calculate_cards_won
+    assert_equal(30, ProcessCards.new("../inputs/4/example_input.txt").calculate_cards_won)
+  end
 end
