61678afaf8ff1d61f783336b24e3660586a0283d
Arek Kita <arkadiusz.kita@lunarlogic.io>
2024-01-30 17:18:25 +0100

    Inline Method: to_s

--- map.rb
+++ map.rb
@@ -4,7 +4,7 @@ class Map
   end
 
   def find_destination(source)
-    to_s.split("\n").reduce(nil) do |_, map_part|
+    @data.split("\n").reduce(nil) do |_, map_part|
       destination_range_start, source_range_start, range_size = map_part.split(" ").collect(&:to_i)
 
       if (source_range_start..source_range_start + range_size - 1).cover?(source)
@@ -13,9 +13,5 @@ class Map
         source
       end
     end
-  end
-
-  def to_s
-    @data
   end
 end
